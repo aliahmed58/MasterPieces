1fe3f3efdd90955dbc65b69ab0f73587a8fcfaf4
Ali Ahmed <alilinkedin77@gmail.com>
2022-12-28 21:42:30 +0500

    get artist by id

--- src/controllers/ArtistsController.js
+++ src/controllers/ArtistsController.js
@@ -1,6 +1,24 @@
 const {convertDate} = require('../../helper')
 const oracledb = require('oracledb')
 
+/*
+ ******** Helper functions below ********
+ * Fetching data from database 
+ * Storing data in database
+*/
+
+const getArtistData = async (connection, artistID) => {
+    const result = await connection.execute(
+        `SELECT * FROM ARTISTS 
+        WHERE ARTISTID = :artistID`,
+        [artistID]
+    )
+
+    return result;
+}
+
+// ----- Exported functions below -----
+
 // show all artists in a list
 const listAllArtists = async (req, res) => {
     let connection;
@@ -20,7 +38,6 @@ const listAllArtists = async (req, res) => {
     finally {
         try {
             await connection.close();
-
             /*
             Result.rows has the following format
                 Example: rows = [ [ 1, 'Kashif', 'London' ] ]
@@ -29,7 +46,6 @@ const listAllArtists = async (req, res) => {
                     1: name
                     2: address
             */
-
             res.render('../src/views/artists/AllArtists', {data: result.rows})
         }
         catch (err) {
@@ -39,8 +55,37 @@ const listAllArtists = async (req, res) => {
 }
 
 // generate report for the given artist id
-const generateReport = (req, res) => {
-    res.render('../src/views/artists/ArtistReport')
+const generateReport = async (req, res) => {
+
+    let connection;
+    let result;
+
+    try {
+        connection = await oracledb.getConnection();
+
+        console.log(req.params.artistID)
+        // get artist data
+        result = await getArtistData(connection, req.params.artistID)
+    }
+    catch (err) {
+        console.log(err)
+    }
+
+    finally {
+        try {
+            console.log(result)
+
+            res.render('../src/views/artists/ArtistReport', {data: result})       
+             
+            // close db connection
+            await connection.close()
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
+
 }
 
 // render form for new artist
@@ -65,7 +110,7 @@ const processForm = async (req, res) => {
         // call insert procedure by passing name and address
         await connection.execute(
             `BEGIN
-                new_artist(:a_name, :a_country, :a_dob);
+                insert_artist (:a_name, :a_country, :a_dob);
             END;
             `,
             {
--- src/database/artistProcedures.js
+++ src/database/artistProcedures.js
@@ -1,16 +1,16 @@
 // insert a new artist procedure
 const insertArtistProcedure = async (connection) => {
     await connection.execute(
-        `CREATE OR REPLACE PROCEDURE new_artist (a_name VARCHAR, a_country VARCHAR, a_dob DATE)
+        `CREATE OR REPLACE PROCEDURE insert_artist (a_name VARCHAR, a_country VARCHAR, a_dob DATE)
         IS
             age number;
         BEGIN
         
-            SELECT (EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM a_dob)) INTO age;
+            age := (EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM a_dob));
             
             INSERT INTO ARTISTS (name, country, dob, age) VALUES (
-                a_name, a_country, '12-JUN-22', age
-            )
+                a_name, a_country, a_dob, age
+            );
 
             COMMIT;
 
