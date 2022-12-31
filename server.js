001b5679551ac28c40db5afb9ccd8e9460f2e540
Ali Ahmed <alilinkedin77@gmail.com>
2023-01-01 02:24:18 +0500

    fixed port

--- server.js
+++ server.js
@@ -40,9 +40,9 @@ app.use('/paintings', paintingRoutes)
 
 initdb()
     .then(() => {
-        app.listen(3000,  () => {
+        app.listen(process.env.PORT,  () => {
             console.log(`connected to database`)
-            console.log(`server listening on port ${3000}`);
+            console.log(`server listening on port ${process.env.PORT}`);
         })
     })
     .catch(err => {
